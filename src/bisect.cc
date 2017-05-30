33ce31fae21fddedf510d4d6e306ff672f344165
Dinesh Bhattarai <dineshbhattrai0@gmail.com>
2017-05-30 14:58:35 +0545

    Fixed the bug noticed by Kapildev Neupane 	modified:   ../src/bisect.cc

--- src/bisect.cc
+++ src/bisect.cc
@@ -1,42 +1,37 @@
 #include <iostream>
 #include <cmath>
+#include <stdexcept>
 
 using namespace std;
+
 float funct(float a){
     return a*a-4;
 }
 
-float bisect(float a, float b,  float (*f)(float), float tolerance = 0.000001){
-	if( a > b){	// we need always a < b
-		float temp = b;
-		b = a;
-		a = temp;
-	}
-	float fa = f(a), fb = f(b), fab = fa * fb;
-
-	if(fab > 0){
+float bisect(float a, float b,  float (*f)(float), float tolerance = 0.0001){
+	if(f(a) * f(b) > 0){
 		throw out_of_range("Solution does not lie in given region");
-	} else if(fa == 0){
+	} else if(f(a) == 0){
 		return a; // a is the solution
-	} else if(fb ==0){
+	} else if(f(b) ==0){
 		return b; // b is the solution.
 	} else {
 		// This region contains the solution
-		float c, lastc;
-		do{
-			c = (a + b) / 2;
-			float fc = f(c);
+		float c, fc;
 
-	        	if(fc == 0){	// We have found a solution.
-	        		break;
-	        	} else if(fc > 0 ){
+		for(int i=0; i < 1000; i++){	// Prevent infinite loop
+			c = (a+b) / 2;
+			fc = f(c);
+
+			if(fc == 0 || fabs(b - a) < tolerance){
+				return c;
+			}
+			if( f(a) * fc > 0){	// Both are same sign i.e. lie on the same side from solution.
 				a = c;
 			} else {
 				b = c;
-			}
-	        	lastc = c;
-	        	c= (a+b) / 2;
-		} while(abs(lastc -c ) > tolerance);
+			}	
+		}
 		return c;
 	}
 }
