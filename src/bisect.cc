975b714607ecbe676206a6f44e38fbb511a3d4f3
Dinesh Bhattarai <dineshbhattrai0@gmail.com>
2017-05-30 15:10:14 +0545

    Decrease one multiplication by swapping a and b.

--- src/bisect.cc
+++ src/bisect.cc
@@ -18,6 +18,13 @@ float bisect(float a, float b,  float (*f)(float), float tolerance = 0.0001){
 	} else {
 		// This region contains the solution
 		float c, fc;
+		
+		// So f(a) lies below the axis, lets swap it. It'll save us one multiplication. Suggestion : Kapildev	
+		if(f(a) < 0) {
+			float temp = b;
+			b = a;
+			a = temp;		
+		}
 
 		for(int i=0; i < 1000; i++){	// Prevent infinite loop
 			c = (a+b) / 2;
@@ -26,7 +33,7 @@ float bisect(float a, float b,  float (*f)(float), float tolerance = 0.0001){
 			if(fc == 0 || fabs(b - a) < tolerance){
 				return c;
 			}
-			if( f(a) * fc > 0){	// Both are same sign i.e. lie on the same side from solution.
+			if(fc > 0){	// Both are same sign i.e. lie on the same side from solution.
 				a = c;
 			} else {
 				b = c;
